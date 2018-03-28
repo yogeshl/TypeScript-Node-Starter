d2f5a094a2959cc00d73c830fb5e6bbbbce0d6ed
yogesh <yogeshlcse@hotmail.com>
2018-03-27 22:39:24 -0400

    Removed unwanted modules and commented code snippet

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,8 +1,4 @@
 "use strict";
-
-import async from "async";
-import request from "request";
-import graph from "fbgraph";
 import { Response, Request, NextFunction } from "express";
 
 
@@ -11,23 +7,9 @@ import { Response, Request, NextFunction } from "express";
  * List of API examples.
  */
 export let getApi = (req: Request, res: Response) => {
-  res.render("api/index", {
-    title: "API Examples"
-  });
+  res.send("hello world");
 };
 
-/**
- * GET /api/facebook
- * Facebook API example.
- */
-export let getFacebook = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.user.tokens.find((token: any) => token.kind === "facebook");
-  graph.setAccessToken(token.accessToken);
-  graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
-    if (err) { return next(err); }
-    res.render("api/facebook", {
-      title: "Facebook API",
-      profile: results
-    });
-  });
+export let getApi1 = (req: Request, res: Response) => {
+  res.send("hello world1");
 };
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,3 @@
-import bcrypt from "bcrypt-nodejs";
-import crypto from "crypto";
 import mongoose from "mongoose";
 
 export type UserModel = mongoose.Document & {
@@ -48,42 +46,10 @@ const userSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-/**
- * Password hash middleware.
- */
-userSchema.pre("save", function save(next) {
-  const user = this;
-  if (!user.isModified("password")) { return next(); }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err); }
-    bcrypt.hash(user.password, salt, undefined, (err: mongoose.Error, hash) => {
-      if (err) { return next(err); }
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-userSchema.methods.comparePassword = function (candidatePassword: string, cb: (err: any, isMatch: any) => {}) {
-  bcrypt.compare(candidatePassword, this.password, (err: mongoose.Error, isMatch: boolean) => {
-    cb(err, isMatch);
-  });
-};
 
 
-/**
- * Helper method for getting user's gravatar.
- */
-userSchema.methods.gravatar = function (size: number) {
-  if (!size) {
-    size = 200;
-  }
-  if (!this.email) {
-    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-  }
-  const md5 = crypto.createHash("md5").update(this.email).digest("hex");
-  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
-};
+
+
 
 // export const User: UserType = mongoose.model<UserType>('User', userSchema);
 const User = mongoose.model("User", userSchema);
